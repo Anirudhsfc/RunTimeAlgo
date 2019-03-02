4915cc7513f64d6e379f3b5aec20bdf36b994ea8
Anirudhsfc <anirudhsfc@gmail.com>
2019-03-02 20:30:44 +0800

    Comments added

--- MakeYourOwnGraph.py
+++ MakeYourOwnGraph.py
@@ -6,16 +6,19 @@ import matplotlib.pyplot as plt
 
 
 def GraphOfMyAlgo(Algo,argumentNumber,arrOfRange):
+    #To check if the user has entered the range that justifies the number of arguments
     if argumentNumber!=len(arrOfRange)/2:
         print("Please enter the range corresponding to the number of arguments")
         exit()
     k=0
     diffArr=[]
+    #To check if there is the same difference between the ranges 
     for i in range(0,argumentNumber):
         diff=arrOfRange[k+1]-arrOfRange[k]
         k=k+2
-        diffArr.append(diff)
+        diffArr.append(diff) #diffArr stores the value of the range
     element=diffArr[0]
+
     for i in range(0,len(diffArr)):
         if diffArr[i]!=element:
             print("Please enter the ranges which have the same difference")
@@ -23,26 +26,25 @@ def GraphOfMyAlgo(Algo,argumentNumber,arrOfRange):
 
     
     k=0
-    argumentArray=[]
-    # print(argumentNumber)
-    # print(len(arrOfRange))
+    argumentArray=[] #stores the value of the  arguments that need to be passed
+    
     for i in range(0,argumentNumber):
-        argument_i=np.arange(arrOfRange[k],arrOfRange[k+1],1)
+        argument_i=np.arange(arrOfRange[k],arrOfRange[k+1],1) #to store the full range of numbers specified for each argument
         k=k+2
         argumentArray.append(argument_i)
-    # print(argumentArray)
-    timeTaken=[]
+    
+    timeTaken=[] #to store the time taken by the Algorithm to run
     for i in range(0,(arrOfRange[1]-arrOfRange[0])):
         l=0
         c=0
-        unpackThis=[]
+        unpackThis=[] #We store the vargument values that need to be passed in an array. Example: if there are two arguments and argument1 takes the values a1,a2,a3,...an and the secomnd argument takes the values b1,b2,b3,b4...,bn then the unpackThis stores one of  {a1,b1},{a2,b2},{a3,b3},...{an,bn} in n  iterations
         while c<argumentNumber:
             unpackThis.append(argumentArray[l][i])
             l=1
             c=c+1
-        #print(unpackThis)
+        
         start=float(t.time())
-        ans=Algo(*unpackThis)
+        ans=Algo(*unpackThis) #we unpack our set of numbers here using the * operator
         end=float(t.time())
         runTime=float(end-start)
         timeTaken.append(runTime)
@@ -51,10 +53,8 @@ def GraphOfMyAlgo(Algo,argumentNumber,arrOfRange):
 
     
     
-    xAxis=np.arange(arrOfRange[0],arrOfRange[1],1)
-    # print(xAxis)
-    # print(timeTaken)
-    # print(len(timeTaken))
+    xAxis=np.arange(arrOfRange[0],arrOfRange[1],1) #We take the x-Axis by default as the range for which the first argument is passed
+    #customising graph
     plt.plot(xAxis,timeTaken,label="Your Algorithm")
     plt.xlabel('Values of Input')
     plt.ylabel('Running time')
@@ -64,10 +64,7 @@ def GraphOfMyAlgo(Algo,argumentNumber,arrOfRange):
     plt.title(' Graph For Running Time of Your Algorithm')
     plt.show()
 
-    # print(argumentArray[0])
-    # print(argumentArray[1])
-    # print(argumentArray[0][0])
-    # print(argumentArray[1][0])
+
 
 
 
--- UserAlgorithm.py
+++ UserAlgorithm.py
@@ -1,4 +1,4 @@
-from MakeYourOwnGraph import *
+from MakeYourOwnGraph import * #user needs to import this line
 
 
 def F1(x,y):
@@ -8,4 +8,6 @@ def F1(x,y):
     else:
         return F1(x-1,y)+F1(x-2,y)
 
-GraphOfMyAlgo(F1,2,[1,30,2,31])
+GraphOfMyAlgo(F1,2,[1,30,2,31]) #user needs to execute this line in his or her algorithm where the first argument is the name of the Algorithm Function, here F1
+                                #the Second argument is the number of arguments that the algorithm of the user inputs
+                                #the third argument is an array of ranges, namely beginning value of range, ending value of range_for_first_argument, beginning value of range_for_second_argument, ending value of range_for_second_argument and so on
